e2888aa0facbdb1d195c6557d9b80e29c347eb14
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2023-12-02 23:12:55 -0500

    small changes

--- day-02/solution.ts
+++ day-02/solution.ts
@@ -9,27 +9,19 @@ function parseGame(gameLog: string) {
   return {
     id: Number(id!),
     subsets: subsets.split(";").map((s) =>
-      [...s.matchAll(SUBSET_RE)].reduce(
+      Array.from(s.matchAll(SUBSET_RE)).reduce(
         (dict, { groups }) => ({
           ...dict,
           [groups?.color!]: Number(groups?.count!),
         }),
-        {
-          red: 0,
-          green: 0,
-          blue: 0,
-        }
+        { red: 0, green: 0, blue: 0 }
       )
     ),
   };
 }
 
 export function part1() {
-  const max: Record<string, number> = {
-    red: 12,
-    green: 13,
-    blue: 14,
-  };
+  const max: Record<string, number> = { red: 12, green: 13, blue: 14 };
 
   return input
     .split("\n")
@@ -45,16 +37,14 @@ export function part1() {
 export function part2() {
   return input
     .split("\n")
-    .map(parseGame)
-    .reduce((sum, game) => {
-      const min = game.subsets.reduce((a, b) => ({
+    .map((game) =>
+      parseGame(game).subsets.reduce((a, b) => ({
         red: Math.max(a.red, b.red),
         green: Math.max(a.green, b.green),
         blue: Math.max(a.blue, b.blue),
-      }));
-
-      return sum + min.red * min.green * min.blue;
-    }, 0);
+      }))
+    )
+    .reduce((sum, min) => sum + min.red * min.green * min.blue, 0);
 }
 
 console.log("Part 1:", part1());
